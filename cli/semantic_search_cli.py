6d1e05466a733922b0ff8e5b71bf7acc61dbf38e
Rounak Kumar Singh <rounak.official.2@gmail.com>
2026-08-10 12:27:27 +0530

    Semantic Search integration starr

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,11 +1,24 @@
+from cli.lib.semantic_search import SemanticSearch, embed_text
 import argparse
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    verify_parser = subparsers.add_parser("verify", help="Verify model loading")
+
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embed text")
+    embed_text_parser.add_argument("text", type=str, help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            ss = SemanticSearch()
+            ss.verify()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
