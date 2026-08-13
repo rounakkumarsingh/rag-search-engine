c9adc1b02fd5aab9fb0c371c9533187241fa1ac6
Rounak Kumar Singh <rounak.official.2@gmail.com>
2026-08-13 11:15:36 +0530

    Precision Calculation according to golden dataset

---
+++ cli/evaluation_cli.py
@@ -0,0 +1,41 @@
+from cli.lib.ranking import RRF_K
+from cli.lib.hybrid_search import HybridSearch
+from cli.lib.movies import load_movies
+from cli.lib.chunked_semantic_search import ChunkedSemanticSearch
+from cli.lib.config import PROJECT_ROOT
+from cli.lib.caches import load_json
+import argparse
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Search Evaluation CLI")
+    parser.add_argument(
+         "--limit",
+        type=int,
+        default=5,
+        help="Number of results to evaluate (k for precision@k, recall@k)",
+    )
+    args = parser.parse_args()
+    limit = args.limit
+
+    golden_dataset = load_json(PROJECT_ROOT / "data" / "golden_dataset.json")
+    hs = HybridSearch(load_movies)
+    print(f"k = {limit}")
+    for test_case in golden_dataset["test_cases"]:
+        query = test_case["query"]
+        results = hs.rrf_search(query, RRF_K, limit)
+        cnt = 0
+        correct_retrieved = []
+        for result in results:
+            if result.document.get_title() in test_case["relevant_docs"]:
+                correct_retrieved.append(result.document.get_title())
+                cnt += 1
+        print(f"- Query: {query}")
+        print(f"  - Precision@{limit}: {round(cnt / len(results), 4)}")
+        print(f"Retrieved: {", ".join(correct_retrieved)}")
+        print(f"Relevant: {", ".join(test_case["relevant_docs"])}")
+
+
+    # run evaluation logic here
+
+if __name__ == "__main__":
+    main()
